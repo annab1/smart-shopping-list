e033e20cd5008a0afa79ffc11bebc03f513a5629
liadgo <liad.goren@gmail.com>
2017-12-16 14:27:39 +0200

    Models

--- src/server/mysite/shopping/models.py
+++ src/server/mysite/shopping/models.py
@@ -1,5 +1,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class ShoppingList(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField('date')
+    user = models.ForeignKey(User)
+
+
+class ProductInstances(models.Model):
+    shopping_list = models.ForeignKey(ShoppingList)
+    product = models.ForeignKey(Product)
+    amount = models.IntegerField()
