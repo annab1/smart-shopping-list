7137ec710ce37b8f3d1231e0e1189c1f8bcf6230
rrashton@gmail.com <roy@bitdam.com>
2017-12-30 20:52:23 +0200

    Added category

--- server/mysite/shopping/serializers.py
+++ server/mysite/shopping/serializers.py
@@ -12,4 +12,4 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ("id", "name")
+        fields = ("id", "name", "category")
