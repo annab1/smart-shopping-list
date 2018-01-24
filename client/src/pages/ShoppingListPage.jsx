79ee97a39977e0ff4764a9f975673f097d355a73
ritabiniash <ritabiniash@gmail.com>
2018-01-24 08:49:45 +0200

    ritas despert try to add list

--- client/src/pages/ShoppingListPage.jsx
+++ client/src/pages/ShoppingListPage.jsx
@@ -1,12 +1,41 @@
 import React, {Component} from "react";
+import Pages from "../constants/Pages";
 
 class ShoppingListPage extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
+
+        const shoppingListViewStore = this.props.shoppingList;
         return (
             <div>
                 Shopping List Page
+                <ul>
+                    { shoppingListViewStore.shoppingList.map(
+                        (name) =>  name={ name },
+                        <li>
+                            <input
+                            type='checkbox'
+                            checked={ product.chosen }
+                            onChange={ this.onProductClicked }
+                            />
+                        </li>
+                    ) }
+
+                </ul>
+                <button onClick={this.addPro}>Add Product</button>
             </div>
         );
+    }
+
+    addPro() {
+        const { shoppingList } = this.state;
+        const { shoppingListViewStore, userViewStore } = this.props;
+        shoppingListViewStore.addProduct()
+            .then(() => {
+                shoppingListViewStore.setCurrentPage(Pages.ShoppingList);
+            });
     }
 }
 
--- client/src/view-stores/ShoppingListViewStore.js
+++ client/src/view-stores/ShoppingListViewStore.js
@@ -1,13 +1,29 @@
 import { observable, action } from "mobx";
 import Pages from "../constants/Pages";
+import Product from "../models/Product";
+import User from "../models/User";
+import ShoppingListApi from "../api/ShoppingListApi";
 
 class ShoppingListViewStore {
   @observable currentPage = Pages.Login;
+  @observable shoppingList = [];
+
+    constructor() {
+        this._api = ShoppingListApi;
+    }
 
   @action.bound
   setCurrentPage(page) {
     this.currentPage = page;
   }
+
+    addProduct(productName) {
+        let newProd  = new Product({name: productName});
+        this.shoppingList.push({
+            product : newProd
+        });
+        return this._api.appProduct(productName);
+    }
 
 }
 
