7d29283eb27551f77d7e372c2d39462703342ec7
rrashton@gmail.com <roy@bitdam.com>
2018-04-22 15:35:29 +0300

    Working proto not tested

--- server/mysite/shopping/views.py
+++ server/mysite/shopping/views.py
@@ -1,10 +1,67 @@
+import pytz
 from django.http import JsonResponse, HttpResponse
-from datetime import datetime
+import pandas as pd
+import datetime
 import time
 # Create your views here.
 from shopping.models import Category, Product, ShoppingList, ProductInstances
 from shopping.serializers import CategorySerializer, ProductSerializer, ProductInstancesSerializer, ShoppingListSerializer
 from django.contrib.auth.models import User
+import pandas as pd
+
+from statsmodels.tsa.arima_model import ARIMA
+from models import UserData, ProductInstances, ShoppingList
+
+
+def aggregate_data(user_id, product_id):
+    '''
+    Aggregates data for specific product given a user id, used for prediction
+    :param user_id: Desired user id
+    :param product_id: Product id to be examined
+    :return: Dataframe containing the data sliced by timeframe
+    '''
+    user_lists = ShoppingList.objects.all().filter(user=user_id)
+    users_products = ProductInstances.objects.all().filter(shopping_list__in=user_lists, product_id=product_id)
+    content = []
+    content.append
+    for single_list in user_lists:
+        product_for_date = [x for x in users_products if x.shopping_list.id == single_list.id]
+        sample_time = (single_list.date - datetime.datetime(1970, 1, 1, tzinfo=pytz.UTC)).total_seconds()
+        #content.append((sample_time, product_for_date[0].amount) if product_for_date else (sample_time, 0))
+        content.append(float(product_for_date[0].amount) if product_for_date else float(0))
+    return content
+    # URLS - https://stackoverflow.com/questions/11697887/converting-django-queryset-to-pandas-dataframe
+    #  https://machinelearningmastery.com/arima-for-time-series-forecasting-with-python
+
+
+def parser(a):
+    return pd.datetime.strptime('2005-05', '%Y-%m')
+
+
+def predict_single_product(user_id, product_id):
+    fa = "c:\\rashti\\mysitelog"+str(user_id)+"_"+str(product_id)+ ".txt"
+    f = open(fa, "wb")
+    data = aggregate_data(user_id, product_id)
+    #a = pd.DataFrame(data)
+    #a.to_csv("c:\\rashti\\mysitecsv.csv")
+    #data = pd.read_csv("c:\\rashti\\mysitecsv.csv", squeeze=False,index=0, parse_dates=[0], date_parser=parser).values
+    f.write(str(data))
+    f.write("\r\n")
+    #TODO: Convert each number to int64 since there's a bug in ARIMA
+    f.write("\r\nData is " + str(data))
+    if len(set(data)) < 2: #0 or 1
+        f.write("Returning due to singular matrix")
+        f.flush()
+        return data[0]
+    model = ARIMA(data, order=(1, 0, 0))
+    f.write("Arima created")
+    f.flush()
+    model_fit = model.fit(disp=0)
+    f.write("\r\nModel was trained")
+    output = model_fit.forecast()
+    f.write("\r\nDone with forecast")
+    f.write(str(output[0]))
+    return output[0][0]
 
 
 def get_categories(request):
@@ -38,7 +95,11 @@ def add_product(request):
 def create_list(request):
     name = request.POST['name']
     user = User.objects.get(id=int(request.user.id))
-    ShoppingList.objects.create(name=name, user=user, date=datetime.now())
+    list_instance = ShoppingList.objects.create(name=name, user=user, date=pd.datetime.now())
+    products = Product.objects.all()
+    for product in products:
+        amount = predict_single_product(request.user.id, product.id)
+        ProductInstances.objects.create(shopping_list=list_instance, product=product, amount=amount)
     return HttpResponse('')
 
 
@@ -58,11 +119,14 @@ def add_some_prodcuts_to_list(id):
 
 
 def generate_list(request):
-    name = time.strftime("%d_%m_%Y")
-    user = User.objects.get(id=1)
-    created_list = ShoppingList.objects.create(name=name, user=user, date=datetime.now())
-    add_some_prodcuts_to_list(created_list.id)
-    serializer = ShoppingListSerializer(created_list)
+    name = request.POST['name']
+    user = User.objects.get(id=int(request.user.id))
+    list_instance = ShoppingList.objects.create(name=name, user=user, date=pd.datetime.now())
+    products = Product.objects.all()
+    for product in products:
+        amount = predict_single_product(request.user.id, product.id)
+        ProductInstances.objects.create(shopping_list=list_instance, product=product, amount=amount)
+    serializer = ShoppingListSerializer(list_instance)
     return JsonResponse(serializer.data, safe=False)
 
 
